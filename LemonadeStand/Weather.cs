bb51cd84e428cb85fdc3a5df47d67d1fb0c0bfc0
entreri514 <148565529+entreri514@users.noreply.github.com>
2023-10-25 13:35:46 -0500

    Completed weather class

--- LemonadeStand/Weather.cs
+++ LemonadeStand/Weather.cs
@@ -7,6 +7,28 @@ using System.Threading.Tasks;
 namespace LemonadeStand
 {
     public class Weather
+        
     {
+        public string dayConditions;
+        public int dayTemperature;
+        public int roll;
+        public Random rand;
+        
+            public Weather(string dayConditions,int dayTemperature)
+        {   
+            rand=new Random(0);
+            roll=rand.Next(3);
+
+            if (roll == 1) dayConditions = "Clear";
+            if (roll == 2) dayConditions = "Cloudy";
+            if (roll == 3) dayConditions = "Rainy";
+
+            rand = new Random(0);
+            dayTemperature = rand.Next(40) + 50;
+
+            
+            
+        }
+
     }
 }
