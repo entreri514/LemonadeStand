49182c4a1ba0d0cbc4f81b2a7446b1d1aae5e1ef
entreri514 <148565529+entreri514@users.noreply.github.com>
2023-10-27 11:54:22 -0500

    Begin debugging Day class

--- LemonadeStand/Day.cs
+++ LemonadeStand/Day.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LemonadeStand;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,19 +9,46 @@ namespace LemonadeStand
 {
     internal class Day
     {
-        public List<Customer>();
-    }   
+        public List<Customer> customers;
 
 
-        public Day[]
+
+        public Day()
         {
-            customers=new List<Customer>();
-    public void StartDay()
-
-    public void CreateCustomers(int numOfCustomers = 25);
-
+            customers = new List<Customer>();
+        }
+        public void StartDay()
 
 
 
+        public void CreateCustomers(int numOfCustomers = 25);
+        {   
+            for (int i = 0; i<numOfCustomers; i++)
+            {   
+            customers.Add(new Customer[]);
+            }
+        }       
+        public double CustomerWalkingby(double lemonadePrice);
+        {
+            int customersBought = 0;
+
+            foreach (Customer customer in customers)
+            { 
+            
+                if (customer.WillBuyLemonade(lemonadePrice))
+                {
+                Console.WriteLine("A customer bought lemonade.");
+
+                customersBought++;
+                
+                }
+                else
+                {
+                Console.WriteLine("A customer walked away.");
+        
+                }
+                
+            }
+        }
     }
 }
